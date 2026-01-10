fe43c7774d29ff5ff1a011dc7b549d866c5b77a6
ubuntuarmin <ubuntuarmin@gmail.com>
2026-01-10 10:33:21 -0600

    Create account.js

---
+++ js/account.js
@@ -0,0 +1,12 @@
+import { userData } from "./auth.js";
+const accountInfo = document.getElementById("accountInfo");
+const totalMinutesEl = document.getElementById("totalMinutes");
+
+export function updateAccount(){
+  accountInfo.innerHTML = `
+    <div>Name: ${userData?.firstName||""} ${userData?.lastName||""}</div>
+    <div>Grade: ${userData?.grade||""}</div>
+    <div>Total Earned: ${userData?.totalEarned||0}</div>
+  `;
+  totalMinutesEl.textContent = userData?.totalMinutes || 0;
+}
